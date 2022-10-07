dabb989eefaed1dc9e2a51e292b30c0c8acb3225
june <jyshine3@gmail.com>
2022-10-07 18:31:19 +0900

    feat(racingCars): 사용자의 입력 값의 이름들이 5자 이하인지 검증

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -10,9 +10,19 @@ public class RacingCars {
 
     public void createCar(String inputValue) {
         String[] splitInputValue = inputValue.split(",");
-        for(String splitValue:splitInputValue){
+        for (String splitValue : splitInputValue) {
+            validationCarName(splitValue);
             carList.add(new Car(splitValue));
         }
 
+    }
+
+    private void validationCarName(String carName) {
+//        TODO: 공백을 카운트로 셀 것인지 확인 및 추가 작업 필요
+//        TODO: 하드코딩하지 않고 상수로 표현하기
+
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 경주할 자동차 이름은 5자 이내로 설정할 수 있습니다.");
+        }
     }
 }
--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -7,13 +9,27 @@ import org.junit.jupiter.api.Test;
 class RacingCarsTest {
 
     @Test
-    void 쉼표_기준으로_분리(){
+    void 쉼표_기준으로_분리() {
         // given
-        String inputValue = "abc, def, asdf, qwert, 123456";
+        String inputValue = "abc,def,asdf,qwert,1234";
         // when
         RacingCars racingCars = new RacingCars();
         racingCars.createCar(inputValue);
         // then
         assertEquals(racingCars.carList.size(), 5);
+    }
+
+    @Test
+    void 자동차_이름_5자_이상_예외처리() {
+        // given
+        String inputValue = "abc,def,asdf,qwert,aaaaaa";
+        // when
+        RacingCars racingCars = new RacingCars();
+
+        // then
+        Throwable thrown = catchThrowable(() -> racingCars.createCar(inputValue));
+        // then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 경주할 자동차 이름은 5자");
+
     }
 }
