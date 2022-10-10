5f342af77f79d075be6ae079f0b289d512da5aed
june <jyshine3@gmail.com>
2022-10-10 22:58:39 +0900

    fix:오류 메시지 수정

--- src/main/java/racingcar/util/Messages.java
+++ src/main/java/racingcar/util/Messages.java
@@ -1,7 +1,7 @@
 package racingcar.util;
 
 public class Messages {
-    public static final String ERROR = "[ERROR] ";
+    public static final String ERROR = "[ERROR]";
     public static final String ERROR_CAR_NAME_LIMIT = ERROR + "경주할 자동차 이름은 5자 이내로 설정할 수 있습니다.";
     public static final String ERROR_CAR_NAME_DUPLICATION = ERROR + "경주할 자동차 이름은 중복될 수 없습니다.";
     public static final String ERROR_RACING_ROUND_NUMBER = ERROR + "시도 횟수는 숫자여야 한다.";
